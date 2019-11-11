59345c575bd6d420d78db1d76492e339558f3a99
an-liu1 <36012691+an-liu1@users.noreply.github.com>
2019-11-10 22:39:41 -0500

    contact form functional

--- js/main.js
+++ js/main.js
@@ -32,22 +32,22 @@
 
     sliderControll = () => {
         var sliderCon = {
-            animation: "slide", 
-            slideshow: true, 
-            slideshowSpeed: 1000, 
-            controlNav: false, 
+            animation: "slide",
+            slideshow: true,
+            slideshowSpeed: 1000,
+            controlNav: false,
             pauseOnHover: "ture",
-            itemWidth: 253, 
-            itemMargin: 31, 
-            prevText: "<", 
-            nextText: ">", 
+            itemWidth: 253,
+            itemMargin: 31,
+            prevText: "<",
+            nextText: ">",
             move: 1,
         }
         $(".experience-history-list").flexslider(sliderCon)
     }
 
     initSmartScroll = () => {
-        if($(window).width() > 420){
+        if ($(window).width() > 420) {
             $.smartscroll({
                 autoHash: true
             });
@@ -55,30 +55,67 @@
     }
 
     closePortrait = () => {
-        $('.closePortrait').on('click', ()=>{
+        $('.closePortrait').on('click', () => {
             $('.my').addClass('hide')
         })
     }
 
     initBacktoTop = () => {
-        $(window).scroll(function(){
+        $(window).scroll(function () {
             if ($(this).scrollTop() > 100) {
                 $('.back-to-top').fadeIn();
             } else {
                 $('.back-to-top').fadeOut();
             }
-        }); 
-        $('.back-to-top').click(function(){
+        });
+        $('.back-to-top').click(function () {
             $("html, body").animate({ scrollTop: 0 }, 1000);
             return false;
         });
+    }
+
+    initContact = () => {
+        $('#contact-form').submit(function () {
+
+            var action = $(this).attr('action');
+
+            $("#message").slideUp(750, function () {
+                $('#message').hide();
+
+                $('#submit')
+                    .before('')
+                    .attr('disabled', 'disabled');
+
+                $.post(action, {
+                    name: $('#name').val(),
+                    email: $('#email').val(),
+                    comments: $('#comments').val(),
+                },
+                    function (data) {
+                        document.getElementById('message').innerHTML = data;
+                        $('#message').slideDown('slow');
+                        $('#cform img.contact-loader').fadeOut('slow', function () {
+                            $(this).remove()
+                        });
+                        $('#submit').removeAttr('disabled');
+                        if (data.match('success') != null) $('#cform').slideUp('slow');
+                    }
+                );
+
+            });
+
+            return false;
+
+        });
     },
 
-    this.initStickyMenu();
+
+        this.initStickyMenu();
     this.initSmoothLink();
     new WOW().init();
     this.sliderControll();
     this.initSmartScroll();
     this.closePortrait();
     this.initBacktoTop();
+    this.initContact();
 })();
